03fba633ae660554a1843e01d6e5a0ef43b2dae9
Dani <kovacs.dnl@gmail.com>
2024-02-29 21:52:53 +0100

    Fix some release deployment issue

--- bundle.ps1
+++ bundle.ps1
@@ -3,8 +3,12 @@ param(
 	[string]$tag
 )
 
+dotnet restore
+dotnet build -c Release --no-restore
+dotnet test --no-build -c Release
+
 # build / publish your app
-dotnet publish -c Release -o ".\publish" 
+dotnet publish -c Release -r win-x64 --self-contained -o ".\publish" 
 
 # build new version and delta updates.
 Write-Host "Creating package" -ForegroundColor Magenta
